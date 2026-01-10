e7ee3e1f4d26c32340e96d1dd4220dd7af3e0213
elochukwuazuwuigne-dot <elochukwuazuwuigne@gmail.com>
2026-01-10 14:24:13 +0000

    Rename getdata.js to getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,3 +3,4 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 // Display last modified date dynamically
 document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
+
